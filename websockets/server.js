1b021d6a91ce05131be6aa2d8979bc95443ecd9f
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-04-06 22:23:01 +0600

    websocket server setup

---
+++ websockets/server.js
@@ -0,0 +1,31 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// 0 :connecting
+// 1 :open, the only state where you can safely .send()
+// 2 :closing
+// 3 :closed
+
+//connection event
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData });
+    wss.clients.forEach((client) => {
+      //   if (client.readyState === 1) client.send(`Server Broadcast: ${message}`);
+
+      if (client.readyState === WebSocket.OPEN)
+        client.send(`Server Broadcast: ${message}`);
+    });
+  });
+
+  socket.on("error", (err) => {
+    console.log(`Error: ${err.message}: ${ip}`);
+  });
+
+  socket.on("close", () => {
+    console.log(`Connection closed: ${ip}`);
+  });
+});
