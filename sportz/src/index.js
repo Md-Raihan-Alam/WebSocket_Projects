bdcc72235083199cdfcec82c61ee9d0374bed981
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-04-16 18:08:16 +0600

    socket establish

--- sportz/src/index.js
+++ sportz/src/index.js
@@ -1,18 +1,31 @@
 import express from "express";
 import { matchRouter } from "./routes/matches.js";
-
+import http from "http";
+import { attachWebSocketServer } from "./ws/server.js";
 const app = express();
 
-const port = 8000;
+const PORT = Number(process.env.PORT) || 8000;
+const HOST = process.env.HOST || "0.0.0.0";
+
+const server = http.createServer(app);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  PORT;
   res.send("Hello from Express server!");
 });
 
 app.use("/matches", matchRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+server.listen(PORT, HOST, () => {
+  const baseUrl =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+  console.log(`Server is running at ${baseUrl}`);
+  console.log(
+    `WebSocket server is running at ${baseUrl.replace("http", "ws")}/ws`
+  );
 });
